1c6aed494ec395b20fbce62ee97c935a5af97971
opilane <opilane@HP-CZC2349HTY>
2024-02-07 16:32:58 +0200

    listid

---
+++ listid/listid/listid.py
@@ -0,0 +1,34 @@
+#Praktiline iseseisevtöö "Listid"
+#1
+#from string import *
+#vokaali=["a","u","i","o","e","ü","ä","ö","õ"]
+#konsonanti="qwrtpsdfghklzxcvbnm"
+#märgid=punctuation
+#v=k=m=t=0
+#tekst=input("Sisesta tekst: ") #"mama"
+#print(tekst)
+#tekst_list=list(tekst) #["m","a","m","a"]
+#print(tekst_list)
+#for element in tekst_list:
+#    if element.lower() in vokaali:
+#        v+=1
+#    elif element.lower() in konsonanti:
+#        k+=1
+#    elif element in märgid:
+#        m+=1
+#    elif element==" ":
+#        t+=1
+#print("Vokaali: ",v)
+#print("Konsonanti: ",k)
+#print("Märgid: ",m)
+#print("Tühikud ",t)
+#2
+nimed=[]
+for i in range(5):
+    nimi=input("Sisesta nimi: ")
+    nimed.append(nimi)
+
+print("Sisestatud: ",nimed)
+nimed.sort()
+print("Sorteeritud: ",nimed)
+print("Viimasena oli lisatud: ",nimi)
