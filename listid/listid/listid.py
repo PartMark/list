65bff09afc1a73be64358a07e30a967769160a48
opilane <opilane@HP-CZC2349HTY>
2024-02-14 12:29:57 +0200

    listid3

--- listid/listid/listid.py
+++ listid/listid/listid.py
@@ -23,28 +23,21 @@
 #print("Märgid: ",m)
 #print("Tühikud ",t)
 #2
-nimed=[]
-for i in range(5):
-    nimi=input("Sisesta nimi: ")
-    nimed.append(nimi)
+#nimed=[]
+#for i in range(5):
+#    nimi=input("Sisesta nimi: ")
+#    nimed.append(nimi)
 
-print("Sisestatud: ",nimed)
-nimed.sort()
-print("Sorteeritud: ",nimed)
-print("Viimasena oli lisatud: ",nimi)
-nimi=input("Mis nimi on vaja asendada? ")
-indeks=nimed.index(nimi)
-uus_nimi=input("Uus nimi: ")
-nimed[indeks]=uus_nimi
-nimed=[uus_nimi if vana_nimi==nimi else vana_nimi for vana_nimi in nimed]
-nimed=set(nimed)
-print(nimed)
-vanused=[]
-for i in range(5):
-    v=int(input("Vanus: "))
-    nimed.append(v)
-sum_=sum(vanused)
-min_=min(vanused)
-max_=max(vanused)
-kesk=sum_/len(vanused)
-print("Keskmine on {kesk}, \nSuurim on {max}, \nVäiksem on {min_}, \nSumma on {sum_}")
+#print("Sisestatud: ",nimed)
+#nimed.sort()
+#print("Sorteeritud: ",nimed)
+#print("Viimasena oli lisatud: ",nimi)
+
+#3
+while True:
+    try:
+        N=int(input("Mitu rida loome? "))
+        break
+    except:
+        print("Vale tüüp")
+S=input("Mis sümbol korrutame? ")
